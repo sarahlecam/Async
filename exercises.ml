37379fbe4c98d54695a0e5bf27c1ea61e18a48d9
Michael Clarkson <clarksmr@gmail.com>
2015-11-03 21:57:52 -0500

    This is version 1, released 11/03/15

---
+++ exercises.ml
@@ -0,0 +1,20 @@
+open Async.Std
+
+let job name t =
+  failwith "TODO"
+
+let both d1 d2 =
+  failwith "TODO"
+
+let fork d f1 f2 =
+  failwith "TODO"
+
+let parallel_map f l =
+  failwith "TODO"
+
+let sequential_map f l =
+  failwith "TODO"
+
+let any ds =
+  failwith "TODO"
+
